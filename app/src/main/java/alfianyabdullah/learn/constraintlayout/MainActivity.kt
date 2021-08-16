3c69045d6cfb613c55320f968befbada7718854d
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2021-08-16 11:16:53 +0700

    initial jetpack componse project :tada:

---
+++ app/src/main/java/alfianyabdullah/learn/constraintlayout/MainActivity.kt
@@ -0,0 +1,38 @@
+package alfianyabdullah.learn.constraintlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import alfianyabdullah.learn.constraintlayout.ui.theme.ConstraintLayoutJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ConstraintLayoutJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ConstraintLayoutJetpackComposeTheme {
+        Greeting("Android")
+    }
+}
