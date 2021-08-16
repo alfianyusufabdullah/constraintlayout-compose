ae1e1e410ff9f67a6bd6bb17bb52703f08ca77b8
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2021-08-16 11:58:00 +0700

    added simple view with Constraint

--- app/src/main/java/alfianyabdullah/learn/constraintlayout/MainActivity.kt
+++ app/src/main/java/alfianyabdullah/learn/constraintlayout/MainActivity.kt
@@ -3,21 +3,25 @@ package alfianyabdullah.learn.constraintlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import alfianyabdullah.learn.constraintlayout.ui.theme.ConstraintLayoutJetpackComposeTheme
+import android.widget.Toast
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ConstraintLayoutJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    ConstraintView("Android")
                 }
             }
         }
@@ -25,14 +29,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ConstraintView(name: String) {
+    val applicationContext = LocalContext.current.applicationContext
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = "ConstraintLayout Compose") })
+    }) {
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            val (welcomeMessage, texting, called) = createRefs()
+            createHorizontalChain(texting, called, chainStyle = ChainStyle.Spread)
+
+            Text(
+                text = "Hai $name! Welcome to the Club",
+                modifier = Modifier.constrainAs(welcomeMessage) {
+                    top.linkTo(parent.top, margin = 32.dp)
+                    centerHorizontallyTo(parent)
+                })
+
+            Button(
+                onClick = {
+                    Toast.makeText(applicationContext, "Message", Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier.constrainAs(texting) {
+                    top.linkTo(welcomeMessage.bottom, margin = 32.dp)
+                }) {
+                Text(text = "Message")
+            }
+
+            Button(
+                onClick = {
+                    Toast.makeText(applicationContext, "Called", Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier.constrainAs(called) {
+                    top.linkTo(welcomeMessage.bottom, margin = 32.dp)
+                }) {
+                Text(text = "Call")
+            }
+
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     ConstraintLayoutJetpackComposeTheme {
-        Greeting("Android")
+        ConstraintView("Android")
     }
 }
